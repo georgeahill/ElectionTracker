2e0d9fd062ffcc7ae1e7a0c71b6d335b3c1bc29f
Elizabeth Sarell <sc18ess@leeds.ac.uk>
2020-11-01 01:13:28 +0000

    json update

--- JSONParsing.py
+++ JSONParsing.py
@@ -5,8 +5,22 @@ import scrape
 
 
 def json_parsing(json_text):
-    pass
+
+    
+
+
+    senateSeats = {"gop":gopSenateSeats,"dem":demSenateSeats,"other":otherSenateSeats}
+    senateStates = {"gop":gopSenateStates,"dem":demSenateStates,"other":otherSenateStates}
+    houseSeats = {"gop":gopHouseSeats,"dem":demHouseSeats,"other":otherHouseSeats}
+    houseStates = {"gop":gopHouseStates,"dem":demHouseStates,"other":otherHouseStates}
+    ev = {"gop":gopEv,"dem":demEv,"other":otherEv}
+    presStates = {"gop":gopPresStates,"dem":demPresStates,"other":otherPresStates}
+    senate = {"seats": senateSeats, "states": senateStates}
+    house = {"seats": houseSeats, "states": houseStates}
+    president = {"ev": ev, "states": presStates}
+    newJson = {"president": president, "house": house, "senate": senate}
+    return json.dumps(newJson)
 
 if __name__ == '__main__':
     json_text = scrape.get_ap_file("https://interactives.ap.org/interactives/2016/general-election/live-data/production/2016-11-08/bop.json")
-    json_parsing(json_text)
+    print(json_parsing(json_text))
